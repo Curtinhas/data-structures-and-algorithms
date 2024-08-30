e971a9649925a5a70af33562317dbc033fc1379a
Curtinhas <carlos_curtinhas@sapo.pt>
2024-08-30 17:41:08 +0100

    O(n^2)

--- Big O/app.js
+++ Big O/app.js
@@ -1,6 +1,8 @@
-function longItems(n){
-  for(let i = 0; i < n; i++){
-    console.log(i);
+function longItems(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
   }
 }
 
