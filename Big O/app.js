672952ce9dc592ef7359a195907681f83e05c1f7
Curtinhas <carlos_curtinhas@sapo.pt>
2024-09-03 15:22:09 +0100

    Pointers

--- Big O/app.js
+++ Big O/app.js
@@ -1,5 +1,14 @@
-function addItems(n) {
+let obj1 = {
+  value: 11,
+};
 
-}
+let obj2 = obj1;
 
-console.log(addItems(5));
+console.log(obj1);
+console.log(obj2);
+
+obj1.value = 22;
+
+
+console.log(obj1);
+console.log(obj2);
