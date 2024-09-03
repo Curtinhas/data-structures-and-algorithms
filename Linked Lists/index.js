fd1695a1d37745e1d468a7713ac981996f08266c
Curtinhas <carlos_curtinhas@sapo.pt>
2024-09-03 22:10:19 +0100

    Linked Lists

---
+++ Linked Lists/index.js
@@ -0,0 +1,14 @@
+// class LinkedList {
+//   constructor(value){
+   // Create a new node 
+//   }
+//   push(value){
+     //Create || add a new node to end
+//   }
+//   unshift(value){
+     //create || add new node to beginning
+//   }
+//   insert(index, value){
+     // Create new node || insert node
+//   }
+// }
